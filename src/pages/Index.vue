a092a5930c86271abe07649e4e07b14ec86a3c78
Steven <hi@stevenaanen.com>
2021-08-29 22:18:11 +0200

    Init using gridsome

---
+++ src/pages/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Jayla's Wishes</h1>
+
+    <ul>
+      <li class="card">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        iusto!
+      </li>
+      <li class="card">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        iusto!
+      </li>
+      <li class="card">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        iusto!
+      </li>
+      <li class="card">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        iusto!
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Jayla's Wishes",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
